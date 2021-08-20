2fdc863d854a90869ead1b284f309f6bbc99c2c1
TaichiShigematsu <shigetaichi1one@gmail.com>
2021-08-21 01:37:02 +0900

    fix: add serveHTTP at cors middleware

--- interface/middleware/cors.go
+++ interface/middleware/cors.go
@@ -2,12 +2,13 @@ package middleware
 
 import "net/http"
 
-func Cors(_ http.Handler) http.Handler {
+func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Headers", "*")
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			next.ServeHTTP(w, r)
 		},
 	)
 }
